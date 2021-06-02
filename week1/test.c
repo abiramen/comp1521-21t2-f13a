11e474a963c1134ad2aa8e745df5687af22496b9
abiramen <7523422+abiramen@users.noreply.github.com>
2021-06-02 23:44:12 +1000

    Add some code to test embeds

--- week1/test.c
+++ week1/test.c
@@ -1,1 +1,73 @@
-//
+// idk here's some code i stole from the tute to test embedding code
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+
+void trim(char *str);
+char **tokenise(char *str, char *sep);
+void freeTokens(char **toks);
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) exit(1);
+
+	char *string = strdup(argv[1]);
+	printf("Input: \"%s\"\n", string);
+	trim(string);
+	printf("Trimmed: \"%s\"\n", string);
+	char **tokens = tokenise(string, " ");
+	for (int i = 0; tokens[i] != NULL; i++)
+		printf("tok[%d] = \"%s\"\n", i, tokens[i]);
+	freeTokens(tokens);
+	return 0;
+}
+
+// trim: remove leading/trailing spaces from a string
+void trim(char *str)
+{
+	int first, last;
+	first = 0;
+	while (isspace(str[first])) first++;
+	last  = strlen(str)-1;
+	while (isspace(str[last])) last--;
+	int i, j = 0;
+	for (i = first; i <= last; i++) str[j++] = str[i];
+	str[j] = '\0';
+}
+
+// tokenise: split a string around a set of separators
+// create an array of separate strings
+// final array element contains NULL
+char **tokenise(char *str, char *sep)
+{
+	// temp copy of string, because strtok() mangles it
+	char *tmp;
+	// count tokens
+	tmp = strdup(str);
+	int n = 0;
+	strtok(tmp, sep); n++;
+	while (strtok(NULL, sep) != NULL) n++;
+	free(tmp);
+	// allocate array for argv strings
+	char **strings = malloc((n+1)*sizeof(char *));
+	assert(strings != NULL);
+	// now tokenise and fill array
+	tmp = strdup(str);
+	char *next; int i = 0;
+	next = strtok(tmp, sep);
+	strings[i++] = strdup(next);
+	while ((next = strtok(NULL,sep)) != NULL) strings[i++] = strdup(next);
+	strings[i] = NULL;
+	free(tmp);
+	return strings;
+}
+
+// freeTokens: free memory associated with array of tokens
+void freeTokens(char **toks)
+{
+	for (int i = 0; toks[i] != NULL; i++) free(toks[i]);
+	free(toks);
+}
